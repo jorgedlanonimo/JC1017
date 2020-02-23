8ebd504c9cd958c530107e3e369a7723de66dbd2
jorgedlanonimo <61190231+jorgedlanonimo@users.noreply.github.com>
2020-02-23 17:59:05 +0100

    Add files via upload

---
+++ src/main/java/Direccion.java
@@ -0,0 +1,11 @@
+public class Direccion {
+    private Integer CodPos;
+    private String provincia;
+    private String poblacion;
+
+    public Direccion(Integer codPos, String provincia, String poblacion) {
+        this.CodPos = codPos;
+        this.provincia = provincia;
+        this.poblacion = poblacion;
+    }
+}
